e55e7309682feee65f54fb024b71695cc11d1b63
Aileen Lizet Cruz Sanchez <116921277+AileenCS@users.noreply.github.com>
2024-09-25 22:26:09 -0700

    Update cliente-socket.py

--- socket/cliente-socket.py
+++ socket/cliente-socket.py
@@ -1,18 +1,18 @@
 import socket
-import threading
+import threading  
 
 def main():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '127.0.0.1'
-    port = 12345
-    client_socket.connect((host, port))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Crea un socket TCP
+    host = '127.0.0.1'  # Define la dirección IP del servidor (localhost)
+    port = 12345  # Define el puerto al que se conectará el cliente
+    client_socket.connect((host, port))  # Conecta el socket del cliente al servidor
 
-    while True:
-        message = input("Enter your message: ")
-        client_socket.sendall(message.encode('utf-8'))
-        data = client_socket.recv(1024)
-        response = data.decode('utf-8')
-        print(f"Server response: {response}")
+    while True: 
+        message = input("Enter your message: ") 
+        client_socket.sendall(message.encode('utf-8'))  # Envía el mensaje al servidor en formato de bytes
+        data = client_socket.recv(1024)  # Recibe la respuesta del servidor
+        response = data.decode('utf-8')  # Decodifica la respuesta de bytes a cadena
+        print(f"Server response: {response}")  # Muestra la respuesta del servidor en la consola
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": 
+    main() 
